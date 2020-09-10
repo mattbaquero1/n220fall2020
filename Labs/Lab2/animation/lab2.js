6958f6b984dab1f7755e8f9862c57ef83474344c
mattbaquero1 <mbaquero@iu.edu>
2020-09-10 19:27:07 -0400

    added links to 2nd task

--- Labs/Lab2/animation/lab2.js
+++ Labs/Lab2/animation/lab2.js
@@ -1,5 +1,20 @@
-let spices = 0;
-function draw() {
-    spices = spices + 1;
-    console.log(spices);
+
+
+function setup() {
+  createCanvas(400, 300);
 }
+
+function draw() {
+  background(0, 0, 0);
+
+  
+  if(mouseX > 200) {
+    fill(255, 0, 0);
+  } else {
+      fill(0, 0, 255)
+  }
+  circle(mouseX, mouseY, 30);
+}
+
+
+
---
+++ Labs/Lab2/animation/worldwrap.js
@@ -0,0 +1,21 @@
+
+
+function setup() {
+    createCanvas(400, 300);
+  }
+  
+  function draw() {
+    background(0, 0, 255);
+  
+    
+    if(mouseX > 200) {
+      fill(255, 0, 0);
+    } else {
+        fill(0, 0, 255)
+    }
+    circle(mouseX, mouseY, 30);
+  }
+  
+  
+  
+  
