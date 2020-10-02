ef6598af12663dfa3e84c5112802f03f63adbd3b
mattbaquero1 <mbaquero@iu.edu>
2020-10-02 15:50:25 -0400

    personal comp

---
+++ Labs/Lab5/functions/personal.js
@@ -0,0 +1,28 @@
+let g;
+let b;
+let e;
+
+
+function setup() {
+  createCanvas(800, 800);
+  g = random(255);
+  b = random(255);
+  e = random(255);
+}
+
+function draw() {
+  background(127);
+  strokeWeight(4);
+  stroke(g, b, e);
+  fill(g, b, e, 150);
+  circle(400, 400, 200, 200);
+}
+
+function mousePressed() {
+  let d = dist(mouseX, mouseY, 400, 400);
+  if (d < 100) {
+    r = random(255);
+    g = random(255);
+    b = random(255);
+  }
+}
