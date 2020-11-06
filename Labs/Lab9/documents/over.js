b605e494a73ff34eb8d53d48ab8a5aee1aa2f92d
mattbaquero1 <mbaquero@iu.edu>
2020-11-06 15:17:59 -0500

    over and out done

---
+++ Labs/Lab9/documents/over.js
@@ -0,0 +1,13 @@
+let dvOver = document.getElementById("over");
+
+dvOver.style.backgroundColor = "#0000FF";
+dvOver.style.height = "100px";
+dvOver.style.width = "100px";
+
+function overBlack() {
+    dvOver.style.backgroundColor = "#000000";
+}
+
+function overBlue() {
+    dvOver.style.backgroundColor = "#0000FF";
+}
---
+++ Labs/Lab9/documents/peak.js
@@ -0,0 +1,12 @@
+let dvSquare = document.getElementById("square");
+
+var mySquare
+
+dvSquare.style.backgroundColor = "#00FF00";
+dvSquare.style.height = "100px";
+dvSquare.style.width = "100px";
+
+function increaseSize() {
+    
+
+}
