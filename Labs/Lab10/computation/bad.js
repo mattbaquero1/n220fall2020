8d1f7f2709f2095c5b80ec1f136c36efbc86b824
mattbaquero1 <mbaquero@iu.edu>
2020-11-20 16:40:25 -0500

    updated bad words

--- Labs/Lab10/computation/bad.js
+++ Labs/Lab10/computation/bad.js
@@ -1,16 +1,29 @@
+var txtInput = document.getElementById("txtInput");
+var wordsFound = document.getElementById("wordsFound");
+var wordCount = document.getElementById("wordCount");
+badWords = "";
+wordsTotal = 0;
+
 function badWordCatcher() {
+    var splitString = txtInput.value.split(" ");
+    txtInput.value = "";
 
-    let wordInput = document.getElementById("wordInput").value;
-    wordInput = wordInput.toLowerCase();
-  
-
-    let arr = wordInput.split(" ");
-
-    let badWords = ["clear", "water", "tires"];
-    
-    let foundBadWords = arr.filter(el => badWords.includes(el));
-    
-    document.getElementById("wordsFound").innerHTML = foundBadWords.join(", ");
-    document.getElementById("wordAmount").innerHTML = foundBadWords.length;
-
+    for(i = 0; i < splitString.length; i++){
+        if(splitString[i] == "clear"){
+            badWords = "Yes";
+            wordsTotal += 1;
+        }else if (splitString[i] == "water") {
+            badWords = "Yes";
+            wordsTotal += 1;
+        }else if (splitString[i] == "tires") {
+            badWords = "Yes";
+            wordsTotal += 1;
+        }else{
+            badWords = "No"
+        }
+        wordsFound.innerHTML = "Are there any bad words? " + badWords;
+        wordCount.innerHTML = "Bad word count: " + wordsTotal;
     }
+    wordsTotal = 0;
+
+}
