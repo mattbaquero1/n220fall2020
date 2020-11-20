cb1efebacd18875acf1038167fb39d9d7ceea013
mattbaquero1 <mbaquero@iu.edu>
2020-11-20 13:25:05 -0500

    bad

--- Labs/Lab10/computation/bad.js
+++ Labs/Lab10/computation/bad.js
@@ -1,14 +1,14 @@
-function badWordCatch() {
+function badWordCatcher() {
 
-    var wordInput = document.getElementById("wordInput").value;
+    let wordInput = document.getElementById("wordInput").value;
     wordInput = wordInput.toLowerCase();
   
 
-    var arr = wordInput.split(" ");
+    let arr = wordInput.split(" ");
 
-    var badWords = ["clear", "water", "tires"];
+    let badWords = ["clear", "water", "tires"];
     
-    var foundBadWords = arr.filter(el => badWords.includes(el));
+    let foundBadWords = arr.filter(el => badWords.includes(el));
     
     document.getElementById("wordsFound").innerHTML = foundBadWords.join(", ");
     document.getElementById("wordAmount").innerHTML = foundBadWords.length;
