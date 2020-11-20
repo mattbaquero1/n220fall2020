8ed83492ddca4f53380225489123b2e6c0b6c775
mattbaquero1 <mbaquero@iu.edu>
2020-11-20 14:49:59 -0500

    fixed math

--- Labs/Lab10/computation/average.js
+++ Labs/Lab10/computation/average.js
@@ -1,10 +1,23 @@
-function calculateAverage() {
-    let numberSeries = document.getElementById("numberSeries").value;
-    var arr = numberSeries.split(" ");
-    var sum = 0;
-    var avg = 0;
+var txtNums = document.getElementById("txtNums");
+var dvAverage = document.getElementById("dvAverage");
+var dvSum = document.getElementById("dvSum");
 
-    for(var i = 0; i < arr.lenght; i++) {
-        sum += +arr[i];
+var avg = 0;
+var sum = 0;
+
+
+
+function calculateData() {
+    var splitString = txtNums.value.split(",");
+    txtNums.value = "";
+
+    for(i=0; i < splitString.length; i++){
+        var txtData = Number(splitString[i]);
+        sum = sum + txtData;
+        avg = sum / splitString.length;
+        dvAverage.innerHTML = "The average: " + avg;
+        dvSum.innerHTML = "The sum: " + sum;
     }
+    avg = 0;
+    sum = 0;
 }
